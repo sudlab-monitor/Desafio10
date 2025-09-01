033c252c5bdbdaa0201439bbb7c245f03e549d09
sudlab-monitor <contacto@sudlab.org>
2025-09-01 07:12:37 -0300

    Add files via upload

---
+++ SPPO.py
@@ -0,0 +1,9 @@
+import geopandas as gpd
+import pandas as pd
+
+file4 = r"Itinerarios_de_Servicos_de_Onibus_Regulares.shp"
+sppo = gpd.read_file(file4)
+
+print("Loading Shapefile...")
+
+shapefile.head()
